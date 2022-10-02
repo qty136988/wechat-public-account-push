609a97fa79417b9ba3a7d49158fef802901f42de
qty136988 <52022794+qty136988@users.noreply.github.com>
2022-10-02 14:41:28 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -54,20 +54,18 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ob7_d6vxQA19T0PP1NDwhQBh6H60',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'cbRh91zEKyd4GgqrRcXJkGCkBFTl5tbJWUXlUvzOLxw',
+      useTemplateId: '7kJZ5yGEPLdAUkg4RJJNC6iMD0MFyFcioFTH4hL0RDQ',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '11-12',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
-        },
+       
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '生日', name: '我', year: '1999', date: '11-12',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '节日', name: '春节', year: '2023', date: '01-22',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
