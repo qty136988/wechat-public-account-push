1a3fdc4fffdfc5d065bee05b9aef477fa881ca30
qty136988 <52022794+qty136988@users.noreply.github.com>
2022-10-02 14:07:17 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -54,7 +54,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ob7_d6vxQA19T0PP1NDwhQBh6H60',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'hmLcoJrxIC9wmBwhwn0w33z0Kq2_2ss26aCp9DniGMo',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '11-12',
       festivals: [
